271096c6386b0b85ad84382cdba08ee48fe19a71
zoharby <Zohar.BenYesha@checkmarx.com>
2017-03-14 16:31:29 +0200

    Name change to JSHigh.js Trying to make medium medium

--- project/src/main/java/Medium.java
+++ project/src/main/java/Medium.java
@@ -5,7 +5,7 @@ public class Medium {
 
     private static String dbms = "12:121:121:12";
 
-     public static void lala(String dbName) throws SQLException {
+     public static void main(String dbName) throws SQLException {
 		DriverManager.getConnection("jdbc:" + dbms + ":" + dbName + ";create=true");
      }
 }
